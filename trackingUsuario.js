a39aabc73ba791cd4b0d80ec225f8891cd22c200
Fabrik Hernandez <fabrik.hernandez@neoris.com>
2021-01-15 00:44:42 -0300

    hola

--- trackingUsuario.js
+++ trackingUsuario.js
@@ -9,3 +9,62 @@ function deteccionUsuario(){
 }
 
 // document.getElementById("deteccionUsuario").addEventListener("load", deteccionUsuario);
+
+function UserAgentHandler(){
+
+    let localUserAgent = localStorage.getItem("actualUser")|| '';
+
+    const loadUser = () =>{
+
+        if(localUserAgent === '')
+        {
+            console.log("Registering new user");
+            var hash = require('object-hash');
+         
+        }else{
+            console.log("User registered as "+localUserAgent);
+        }
+    }
+
+}
+
+cle 
+    return(
+        <div>
+            {loadUser}
+        </div>
+    );
+
+
+
+
+var hash = require('object-hash');
+let hashValue = hash({user : navigator.userAgent,date : Date.now()});
+localStorage.setItem("actualUser",hashValue);    
+
+
+
+
+
+
+function UserAgentHandler(){
+ 
+    let localUserAgent = localStorage.getItem("actualUser")|| '';
+   
+ 
+    const loadUser = () =>{       
+        
+        if(localUserAgent === '')        
+        {
+            console.log("Registering new user");
+            var hash = require('object-hash');
+            let hashValue = hash({user : navigator.userAgent,date : Date.now()});
+            localStorage.setItem("actualUser",hashValue);
+            
+ 
+        }else{
+            console.log("User registered as "+localUserAgent);
+        }     
+
+    }
+}
